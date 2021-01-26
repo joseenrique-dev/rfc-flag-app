0eb155fee88fbbd06261cd9dce5d0c563d718665
José Enrique <otipip90@gmail.com>
2021-01-26 17:20:19 -0500

    working on country selected design component.

--- src/components/Country-selected/index.js
+++ src/components/Country-selected/index.js
@@ -12,29 +12,29 @@ export default function CountrySelected(
     ) {
         
     return (
-        <div>
-            <Wrapper>
-                <img src={flag} alt=""/>
-                <h2>{name}</h2>
+        <div className="country-selected-box">            
+            <img src={flag} alt="" className="img-country-selected"/>
+            <div>
                 <div className="grid">
                     <div>
+                        <h2>{name}</h2>
                         <p><strong>Native Name:</strong> {nativeName}</p>
                         <p><strong>Population:</strong> {population}</p>
                         <p><strong>Region:</strong> {region}</p>
                         <p><strong>Sub Región:</strong> {subRegion}</p>
                         <p><strong>Capital:</strong> {capital}</p>
                     </div>
-                </div>
-                <div>
-                    <p><strong>Top Level Domain:</strong> {topLevelDomain}</p>
-                    <p><strong>Currencies:</strong> {currencies.map((item)=> <span>{item.name} </span>)}</p>
-                    <p><strong>Languages:</strong> {languages.map((item) => <span className="languages">{item.name}</span>)}</p>
+                    <div>
+                        <p><strong>Top Level Domain:</strong> {topLevelDomain}</p>
+                        <p><strong>Currencies:</strong> {currencies.map((item)=> <span>{item.name} </span>)}</p>
+                        <p><strong>Languages:</strong> {languages.map((item) => <span className="languages">{item.name}</span>)}</p>
+                    </div>
                 </div>
                 <p><strong>Borders:</strong></p>
                 <div className="border-box">
                     {borders.map(data=> <span className="border-item">{data} </span>)}
                 </div>
-            </Wrapper>
+            </div>            
         </div>
     )
 }
